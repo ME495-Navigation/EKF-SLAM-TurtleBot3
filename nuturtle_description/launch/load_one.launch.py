3ca0f1bb32ffde6e8beff00e3e3085fdf7587147
Abhishek <241abhishek@gmail.com>
2024-01-07 08:10:41 -0600

    Added color argument to the launchfile.
    Made channges to allow dynamic reassignment of namespace,
    frame_prefix and the rviz config file.

--- nuturtle_description/launch/load_one.launch.py
+++ nuturtle_description/launch/load_one.launch.py
@@ -1,8 +1,8 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument, Shutdown
+from launch.actions import DeclareLaunchArgument, Shutdown, SetLaunchConfiguration
 from launch.substitutions import Command, PathJoinSubstitution, TextSubstitution, LaunchConfiguration, EqualsSubstitution
-from launch.conditions import LaunchConfigurationEquals, IfCondition
+from launch.conditions import IfCondition
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
@@ -13,27 +13,41 @@ def generate_launch_description():
         DeclareLaunchArgument(name="use_rviz", default_value="true",
                               description="true (default): start rviz, otherwise don't start rviz"),
 
+        DeclareLaunchArgument(name="color", default_value="purple",
+                              description="purple (default): set the color of the robot",
+                              choices = ["red", "green", "blue", "purple"]),
+        
+        SetLaunchConfiguration(name="rviz_config",
+                               value = [TextSubstitution(text="basic_"), LaunchConfiguration("color"), TextSubstitution(text=".rviz")]),
+
         Node(package="joint_state_publisher",
              executable="joint_state_publisher",
+             namespace= LaunchConfiguration("color"),
              condition= IfCondition(
                     EqualsSubstitution(LaunchConfiguration("use_jsp"), "true"))
              ),
         Node(
             package="robot_state_publisher",
             executable="robot_state_publisher",
+            namespace= LaunchConfiguration("color"),
             parameters=[
                 {"robot_description" :
                  Command([TextSubstitution(text="xacro "),
                           PathJoinSubstitution(
-                              [FindPackageShare("nuturtle_description"), "urdf", "turtlebot3_burger.urdf.xacro"])])}
+                              [FindPackageShare("nuturtle_description"), "urdf", "turtlebot3_burger.urdf.xacro"])]),
+                 "frame_prefix" : [LaunchConfiguration("color"), TextSubstitution(text="/")]
+                              }
             ]
             ),
         Node(
             package="rviz2",
             executable="rviz2",
+            namespace= LaunchConfiguration("color"),
             arguments=["-d",
                        PathJoinSubstitution(
-                           [FindPackageShare("nuturtle_description"), "config", "basic_purple.rviz"])],
+                           [FindPackageShare("nuturtle_description"), "config", LaunchConfiguration("rviz_config")]),
+                        "-f", [LaunchConfiguration("color"), TextSubstitution(text="/"), TextSubstitution(text="base_footprint")]   
+                           ],
             condition=IfCondition(
                     EqualsSubstitution(LaunchConfiguration("use_rviz"), "true")),
             on_exit=Shutdown()
