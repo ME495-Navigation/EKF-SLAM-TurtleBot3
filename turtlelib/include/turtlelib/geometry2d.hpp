20c6a4de8b9e13e8b3e2ac625eb45b5d872504bc
Abhishek <241abhishek@gmail.com>
2024-01-09 19:57:34 -0600

    Configured turtlelib as per Task B guidelines.
    Added all the required header files and created
    empty cpp files as placeholders. Partially
    set up the CMakeLists.txt file. Added some function
    impplementations in geometry2d.hpp.

--- turtlelib/include/turtlelib/geometry2d.hpp
+++ turtlelib/include/turtlelib/geometry2d.hpp
@@ -5,6 +5,7 @@
 
 
 #include <iosfwd> // contains forward definitions for iostream objects
+#include <cmath>
 namespace turtlelib
 {
     /// \brief PI.  Not in C++ standard until C++20.
@@ -21,6 +22,7 @@ namespace turtlelib
     /// if given a compile-time constant as input
     constexpr bool almost_equal(double d1, double d2, double epsilon=1.0e-12)
     {
+        return std::abs(d1-d2)<epsilon;
     }
 
     /// \brief convert degrees to radians
@@ -28,6 +30,7 @@ namespace turtlelib
     /// \returns radians
     constexpr double deg2rad(double deg)
     {
+        return deg*(PI/180.0);
     }
 
     /// \brief convert radians to degrees
@@ -35,6 +38,7 @@ namespace turtlelib
     /// \returns the angle in degrees
     constexpr double rad2deg(double rad)
     {
+        return rad*(180.0/PI);
     }
 
     /// \brief wrap an angle to (-PI, PI]
