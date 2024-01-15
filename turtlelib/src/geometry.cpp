d6457256c1f27b214168f721e7c89286c65c75fd
Abhishek <241abhishek@gmail.com>
2024-01-14 19:38:29 -0600

    Implemented is overload and added tests.

--- turtlelib/src/geometry.cpp
+++ turtlelib/src/geometry.cpp
@@ -21,4 +21,47 @@ std::ostream & operator<<(std::ostream & os, const Point2D & p)
     return os << "[" << p.x << " " << p.y << "]";
 }
 
+std::istream & operator>>(std::istream & is, Point2D & p)
+{
+    char next = is.peek();
+
+    if(next == '[')
+    {
+        is.get();
+        is >> p.x;
+        is >> p.y;
+        is.get();
+    }
+    else
+    {
+        is >> p.x >> p.y;
+    }
+
+    return is;
+}
+
+
+std::ostream & operator<<(std::ostream & os, const Vector2D & v)
+{
+    return os << "[" << v.x << " " << v.y << "]";
+}
+
+std::istream & operator>>(std::istream & is, Vector2D & v)
+{
+    char next = is.peek();
+
+    if(next == '[')
+    {
+        is.get();
+        is >> v.x;
+        is >> v.y;
+        is.get();
+    }
+    else
+    {
+        is >> v.x >> v.y;
+    }
+
+    return is;
+}
 }
--- turtlelib/tests/test_geometry2.cpp
+++ turtlelib/tests/test_geometry2.cpp
@@ -25,4 +25,45 @@ TEST_CASE( "output stream 2d point", "[2d point output stream]" ) {
     os << point;
     REQUIRE( os.str() == "[5.7 6.3]" );
 }
+
+TEST_CASE( "output stream vector", "[2d vector output stream]" ) {
+    Vector2D vector;
+    vector.x = 2.7;
+    vector.y = 0.93;
+    std::stringstream os;
+    os << vector;
+    REQUIRE( os.str() == "[2.7 0.93]" );
+}
+
+TEST_CASE( "input stream 2d point", "[2d point input stream]" ) {
+    Point2D point_1, point_2;
+    std::stringstream is_1,is_2;
+
+    is_1 << "0.7 4.3";
+    is_1 >> point_1;
+
+    is_2 << "[9.5 1.32]";
+    is_2 >> point_2;
+
+    REQUIRE( point_1.x == 0.7 );
+    REQUIRE( point_1.y == 4.3 );
+    REQUIRE( point_2.x == 9.5 );
+    REQUIRE( point_2.y == 1.32 );
+}
+
+TEST_CASE( "input stream vector", "[2d vector input stream]" ) {
+    Vector2D vector_1, vector_2;
+    std::stringstream is_1,is_2;
+
+    is_1 << "2.7 0.93";
+    is_1 >> vector_1;
+
+    is_2 << "[2.5 0.97]";
+    is_2 >> vector_2;
+
+    REQUIRE( vector_1.x == 2.7 );
+    REQUIRE( vector_1.y == 0.93 );
+    REQUIRE( vector_2.x == 2.5 );
+    REQUIRE( vector_2.y == 0.97 );
+}
 }
