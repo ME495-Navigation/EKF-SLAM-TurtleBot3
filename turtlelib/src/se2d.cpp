0f942c22774696be571a877d8f1a390b50137cdc
Abhishek <241abhishek@gmail.com>
2024-01-16 16:50:16 -0600

    Added Transform2D constructor implementations.

--- turtlelib/src/se2d.cpp
+++ turtlelib/src/se2d.cpp
@@ -1,6 +1,7 @@
 #include <turtlelib/se2d.hpp>
 #include <iostream>
-
+#include <vector>
+#include <cmath>
 
 namespace turtlelib{
 std::ostream & operator<<(std::ostream & os, const Twist2D & tw)
@@ -26,5 +27,44 @@ std::istream & operator>>(std::istream & is, Twist2D & tw)
     }
 
     return is;
+}
+
+Transform2D::Transform2D() 
+{
+    // Declare a 2D vector of integers
+    std::vector<std::vector<double>> trans2d;
+    trans2d = {{1.0,0.0,0.0},{0.0,1.0,0.0},{0.0,0.0,1.0}};
+}
+
+Transform2D::Transform2D(Vector2D trans)
+{
+    // Declare a 2D vector of integers
+    std::vector<std::vector<double>> trans2d;
+    trans2d = {{1.0,0.0,trans.x},{0.0,1.0,trans.y},{0.0,0.0,1.0}};
+
+}
+
+Transform2D::Transform2D(double radians)
+{
+    // Declare a 2D vector of integers
+    std::vector<std::vector<double>> trans2d;
+
+    // Calculate sine and cosine
+    double sine = sin(radians);
+    double cosine = cos(radians);
+
+    trans2d = {{cosine,-sine,0.0},{sine,cosine,0.0},{0.0,0.0,1.0}};
+}
+
+Transform2D::Transform2D(Vector2D trans, double radians)
+{
+    // Declare a 2D vector of integers
+    std::vector<std::vector<double>> trans2d;
+
+    // Calculate sine and cosine
+    double sine = sin(radians);
+    double cosine = cos(radians);
+
+    trans2d = {{cosine,-sine,trans.x},{sine,cosine,trans.y},{0.0,0.0,1.0}};
 }
 }
